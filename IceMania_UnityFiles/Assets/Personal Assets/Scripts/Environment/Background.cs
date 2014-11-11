02e4bf4bbc2da4b754e8df8253f2a07b0b7ba5dc
MagaBenG <blackpensoftware@gmail.com>
2014-11-11 12:40:39 -0500

    BackGround Color System

---
+++ IceMania_UnityFiles/Assets/Personal Assets/Scripts/Environment/Background.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	/* Object Declaration*/
+	public GameObject PlayerCamera;
+
+	/* Color Creation */
+	public Color background = Color.black;
+
+	void Start(){
+
+		/* Sets The Background Color For The Whole View Of The Camera */
+		PlayerCamera.camera.backgroundColor = background;
+
+	}
+
+
+	void Update() {
+	
+
+
+	}
+}
---
+++ IceMania_UnityFiles/Assets/Personal Assets/Scripts/Environment/Clouds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clouds : MonoBehaviour {
+
+	/* Objet Creation */
+	public GameObject PlayerCamera;
+
+	/* Texture Declaration */
+	public Texture CloudTexture;
+
+	/* Cloud Positions */
+	public int cloud_x = 0;
+	public int cloud_y = 0;
+	public int cloud_width = 64;
+	public int couud_height = 32;
+
+	void OnGUI() {
+		if (!CloudTexture) {
+			Debug.LogError("Assign a Cloud Texture in the inspector.");
+			return;
+		}
+		GUI.DrawTexture(new Rect(cloud_x, cloud_y, cloud_width, couud_height), CloudTexture, ScaleMode.ScaleAndCrop, true, 20.0F);
+	}
+}
